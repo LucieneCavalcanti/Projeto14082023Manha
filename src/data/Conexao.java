8f5aa6735530fc588566fbb8d0101c592dc1f350
lucienecavalcanti <lucienecavalcanti@live.com>
2023-08-21 11:31:03 -0300

    [UPDATES]

--- src/data/Conexao.java
+++ src/data/Conexao.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conexao {
     private Connection conexao;
     public Conexao() throws Exception{
-        String url="jdbc:sqlserver://localhost:1433;databaseName=bdExemploManha";
+        String url="jdbc:sqlserver://localhost:1433;databaseName=bdExemploManha;encrypt=true;trustServerCertificate=true";
         String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
         String usuarioBanco="UsuarioJava";
         String senhaBanco = "Fatec2023";
